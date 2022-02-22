ca747c30ef8add234e85fdd080d8e8b5d8a5d1aa
thanakritcankha <68786794+thanakritcankha@users.noreply.github.com>
2022-02-22 11:48:53 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,124 @@
 <script setup>
-import { ref, reactive, computed } from 'vue'
-import Swal from 'sweetalert2'
+import { ref, reactive, computed } from "vue";
+import Swal from "sweetalert2";
 
-const baseUSD = reactive({
-  USD: 1,
-  THB: 32.4575,
-  JPY: 115.688,
-  CNY: 6.35765,
-  KRW: 1197.14,
-})
+const baseUSD = reactive([
+  {
+    name: "USA",
+    rate: 1,
+    img: "images/USA.png",
+  },
+  {
+    name: "THA",
+    rate: 32.4575,
+    img: "images/THA.png",
+  },
+  {
+    name: "JPY",
+    rate: 115.688,
+    img: "images/JPY.png",
+  },
+  {
+    name: "CNY",
+    rate: 6.35765,
+    img: "images/CNY.png",
+  },
+  {
+    name: "KRW",
+    rate: 1197.141,
+    img: "images/KRW.png",
+  },
+]);
+const crypto = reactive([
+  {
+    name: "ETH",
+    rate: 0.000379,
+    img: "images/ETH.png",
+  },
+  {
+    name: "BTC",
+    rate: 0.0000261,
+    img: "images/BTC.png",
+  },
+  {
+    name: "DOGE",
+    rate: 13.68,
+    img: "images/DOGE.png",
+  },
+  {
+    name: "BNB",
+    rate: 0.002623,
+    img: "images/BNB.png",
+  },
+]);
 
 const currenFrom = ref("");
 const currenTo = ref("");
 const amount = ref("");
 const tranferAmount = ref("");
+
 const calExchange = (from, to) => {
   // console.log(`from ${from} : to ${to} : amount ${amount.value}`)
   // console.log(baseUSD[`${to}`])
-  console.log(amount.value != '')
-  if (amount.value != '') {
-    tranferAmount.value = (amount.value * baseUSD[`${to}`]) / baseUSD[`${from}`]
-    document.getElementById('result').value = tranferAmount.value
+  // console.log(amount.value != "");
+  // console.log(baseUSD);
+  let countryTo = baseUSD.filter((value) => value.name == [`${to}`])[0];
+  let countryFrom = baseUSD.filter((value) => value.name == [`${from}`])[0];
+  // console.log(countryTo.rate);
+  // console.log(countryFrom.rate);
+  // baseUSD.filter(value=> value.name[`${to}`] )
+  if (amount.value != "") {
+    tranferAmount.value = (amount.value * countryTo.rate) / countryFrom.rate;
+    document.getElementById("result").value = tranferAmount.value;
   } else {
     Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Please fill out the information completely.',
-    })
+      icon: "error",
+      title: "ขออภัย...",
+      text: "โปรดกรอกข้อมูลในช่องว่างให้เรียบร้อย",
+    });
   }
-}
+};
+
+const calExchangCrypto = (from, to) => {
+  let countryTo = crypto.filter((value) => value.name == [`${to}`])[0];
+  let countryFrom = crypto.filter((value) => value.name == [`${from}`])[0];
+  if (amount.value != "") {
+    tranferAmount.value = (amount.value * countryTo.rate) / countryFrom.rate;
+    document.getElementById("result").value = tranferAmount.value;
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "ขออภัย...",
+      text: "โปรดกรอกข้อมูลในช่องว่างให้เรียบร้อย",
+    });
+  }
+};
 
 const switchCurren = (from, to) => {
-  let swap = from
-  let swap2 = to
-  currenFrom.value = swap2
-  currenTo.value = swap
-  calExchange(swap2, swap)
-}
+  let swap = from;
+  let swap2 = to;
+  currenFrom.value = swap2;
+  currenTo.value = swap;
+  calExchange(swap2, swap);
+};
+const remainingExchange = computed(() =>
+  baseUSD.filter((currency) => currency.name != currenFrom.value)
+);
+const isToggle = ref(false);
+const showCrypto = () => (isToggle.value = !isToggle.value);
+
+const remainingExchangeCrypto = computed(() =>
+  crypto.filter((currency) => currency.name != currenFrom.value)
+);
 </script>
 
 <template>
-  <!-- It's a navigation bar. -->
-  <div class="flex">
-  <div class="flex relative">
-      <div class="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto border-r">
-        <h2 class="text-3xl font-semibold text-center">Mod X-Change</h2>
+  <div style="text-align: center; margin-top: 2rem">
+    <button class="btn btn-info" @click="showCrypto">cryptocurrency</button>
+  </div>
 
-
-        <div class="flex flex-col justify-between mt-6">
-          <aside>
-            <ul>
-              <li>
-                <a class="flex items-center px-4 py-2 text-gray-700 bg-gray-100 rounded-md " href="#exchange">
-                  <img src="../public/img/coin.png" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                  
-
-                  <span class="mx-4 font-medium">Exchanges</span>
-                </a>
-              </li>
-
-              <li>
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 rounded-md hover:bg-gray-200" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                  </svg>
-
-                  <span class="mx-4 font-medium">Card Flip</span>
-                </a>
-              </li>
-            </ul>
-
-          </aside>
-          
-        </div>
-      </div>
-      </div>
-      <div class="container mx-auto flex justify-center mt-10" id="exchange">
-    <div class="p-10 card bg-base-200 flex justify-center">
+  <div class="container mx-auto flex justify-center mt-10" id="exchange">
+    <div class="p-10 card bg-base-200 flex justify-center" v-if="!isToggle">
       <label class="input-group">
         <span>Amount</span>
         <input
@@ -108,9 +143,14 @@ const switchCurren = (from, to) => {
           v-model="currenFrom"
           class="select select-bordered w-full"
         >
-          <option v-for="(value, key) in baseUSD">{{ key }}</option>
+          <option v-for="(value, key, index) in baseUSD">
+            {{ value.name }}
+          </option>
         </select>
-        <button @click="switchCurren(currenFrom, currenTo)" class="btn btn-info">
+        <button
+          @click="switchCurren(currenFrom, currenTo)"
+          class="btn btn-secondary"
+        >
           Switch
         </button>
         <select
@@ -119,7 +159,9 @@ const switchCurren = (from, to) => {
           v-model="currenTo"
           class="select select-bordered w-full"
         >
-          <option v-for="(value, key) in baseUSD">{{ key }}</option>
+          <option v-for="(value, key, index) in remainingExchange">
+            {{ value.name }}
+          </option>
         </select>
         <div class="form-control col-span-3">
           <label class="label place-content-center">
@@ -136,7 +178,7 @@ const switchCurren = (from, to) => {
             <span>{{ currenTo }}</span>
             <button
               @click="calExchange(currenFrom, currenTo)"
-              class="btn btn-info mx-auto"
+              class="btn btn-primary mx-auto"
             >
               Convert
             </button>
@@ -144,27 +186,8 @@ const switchCurren = (from, to) => {
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="container mx-auto flex justify-center mt-8">
-    <div class="px-10 card bg-base-200 flex">
-      <h1 class="flex justify-center mt-3">Exchange Rates</h1>
-      <div class="grid grid-cols-2 gap-4 mt-3">
-        <p class="px-16 col-start-1">Currency</p>
-        <p class="px-16 col-start-2">Amount</p>
-      </div>
-      <div
-        class="grid grid-cols-2 gap-4 mt-3 mb-3 text-center"
-        v-for="(value, key) in baseUSD"
-      >
-        <p>{{ key }}</p>
-        <p>{{ value }}</p>
-      </div>
-    </div>
-  </div>
-</div>
-  <!-- <div class="container mx-auto flex justify-center mt-10" id="exchange">
-    <div class="p-10 card bg-base-200 flex justify-center">
+    <div class="p-10 card bg-base-200 flex justify-center" v-else>
       <label class="input-group">
         <span>Amount</span>
         <input
@@ -189,9 +212,14 @@ const switchCurren = (from, to) => {
           v-model="currenFrom"
           class="select select-bordered w-full"
         >
-          <option v-for="(value, key) in baseUSD">{{ key }}</option>
+          <option v-for="(value, key, index) in crypto">
+            {{ value.name }}
+          </option>
         </select>
-        <button @click="switchCurren(currenFrom, currenTo)" class="btn btn-info">
+        <button
+          @click="switchCurren(currenFrom, currenTo)"
+          class="btn btn-secondary"
+        >
           Switch
         </button>
         <select
@@ -200,7 +228,9 @@ const switchCurren = (from, to) => {
           v-model="currenTo"
           class="select select-bordered w-full"
         >
-          <option v-for="(value, key) in baseUSD">{{ key }}</option>
+          <option v-for="(value, key, index) in remainingExchangeCrypto">
+            {{ value.name }}
+          </option>
         </select>
         <div class="form-control col-span-3">
           <label class="label place-content-center">
@@ -216,8 +246,8 @@ const switchCurren = (from, to) => {
             />
             <span>{{ currenTo }}</span>
             <button
-              @click="calExchange(currenFrom, currenTo)"
-              class="btn btn-info mx-auto"
+              @click="calExchangCrypto(currenFrom, currenTo)"
+              class="btn btn-primary mx-auto"
             >
               Convert
             </button>
@@ -236,25 +266,62 @@ const switchCurren = (from, to) => {
       </div>
       <div
         class="grid grid-cols-2 gap-4 mt-3 mb-3 text-center"
-        v-for="(value, key) in baseUSD"
+        v-for="(value, key, index) in baseUSD"
+        v-if="!isToggle"
       >
-        <p>{{ key }}</p>
-        <p>{{ value }}</p>
+        <p>
+          <span class="avatar">
+            <span class="w-8 rounded flex items-center mr-10">
+              <img :src="`${value.img}`" class="mr-2" />
+              <p>{{ value.name }}</p>
+            </span>
+          </span>
+        </p>
+        <p>
+          {{
+            new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+              minimumFractionDigits: 5,
+            }).format(value.rate)
+          }}
+        </p>
+      </div>
+
+      <div
+        class="grid grid-cols-2 gap-4 mt-3 mb-3 text-center"
+        v-for="(value, key, index) in crypto"
+        v-else
+      >
+        <p>
+          <span class="avatar">
+            <span class="w-8 rounded flex items-center mr-10">
+              <img :src="`${value.img}`" class="mr-2" />
+              <p>{{ value.name }}</p>
+            </span>
+          </span>
+        </p>
+        <p>
+          {{
+            new Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+              minimumFractionDigits: 5,
+            }).format(value.rate)
+          }}
+        </p>
       </div>
     </div>
-  </div> -->
-
-
-      
+  </div>
 </template>
 
 <style>
-input[type='number']::-webkit-inner-spin-button,
-input[type='number']::-webkit-outer-spin-button {
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
   -webkit-appearance: none;
   margin: 0;
 }
-input[id='result'] {
+input[id="result"] {
   pointer-events: none;
 }
 </style>
