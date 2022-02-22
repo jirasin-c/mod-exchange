fdcc6a5112735eece7e30cce200fd3d922af96cc
Thatphum Kongnithichalerm <antyoung@Thatphums-MacBook-Pro.local>
2022-02-22 15:28:15 +0700

    dark mode

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Navbar from './components/Navbar.vue'
 import './index.css'
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
-const app = createApp(App)
+createApp(App).use(VueSweetalert2).mount('#app');
 
-app.use(VueSweetalert2);
+createApp(Navbar).mount('#navbar');
 
-app.mount('#app');
